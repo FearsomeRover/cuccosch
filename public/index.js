95c426bab185ed83a7c9e5454618dcdf55277bda
FearsomeRover <bujgergo@gmail.com>
2023-03-23 19:24:20 +0100

    changes in html

--- public/index.js
+++ public/index.js
@@ -1,31 +1,9 @@
 const express = require('express');
 const app = express();
-app.use('/static', express.static('static'));
+app.use(express.static('public'))
 
-app.get('/', (req, res, next) => {
-  res.sendFile(__dirname + "/static/index.html");
-});
 
-app.get('/tagok', (req, res, next) => {
-  res.sendFile(__dirname + "/static/tagok.html");
-});
-
-app.get('/tagok/uj', (req, res, next) => {
-  res.sendFile(__dirname + "/static/ujtag.html");
-});
-
-app.listen(3000, '192.168.2.220', () => {
+app.listen(3000, () => {
   console.log('Running on :3000');
 });
 
-app.get('/style.css', (req, res) => {
-  res.sendFile(__dirname + "/static/style.css");
-
-});
-app.get('/logo.png', (req, res) => {
-  res.sendFile(__dirname + "/static/logo.png");
-});
-
-app.get('/images/cuccosch.png', (req, res) => {
-  res.sendFile(__dirname + "/images/cuccosch.png");
-});
